2a53f65d48f89926d3ef7071a21dd5a311a1fef2
itlancer999 <it.lancer999@gmail.com>
2024-06-19 10:50:01 -0400

    work update

--- src/Home.js
+++ src/Home.js
@@ -120,7 +120,7 @@ export const Home = () => (
                         <h1 className="header faint">Maryam M.</h1>
                         <h1 className="header-under">UX Designer</h1>
                         <h2 className="tag">I <span className="highlight">design</span>, <span className="highlight">draw</span> and <span className="highlight">code</span>.</h2>
-                        <h2 className="tag">Currently, <span className="highlighted">Volunteering</span> with North East Farmers of Color for their <span className="highlighted">Research Project</span></h2>
+                        <h2 className="tag"><span className="highlighted">Currently, Volunteering with North East Farmers of Color for their Research Project </span></h2>
                 </main>
               </Col>
   
